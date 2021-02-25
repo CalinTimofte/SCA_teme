de051db769b8297bbfbaac0f7acc9893487e5842
Calin Timofte <calin.timofte@yahoo.com>
2021-02-25 11:48:17 +0200

    Removes useless merchant copy/pasted code Adds future merchant function headers

--- e-commerce/Merchant.py
+++ e-commerce/Merchant.py
@@ -1,10 +1,31 @@
 import socket
 
 
+def recv_message_1(client_conn):
+    pass
+
+
+def send_message_2(client_conn):
+    pass
+
+
+def send_message_4(pg_socket):
+    pass
+
+
+def recv_message_5(pg_socket):
+    pass
+
+
+def send_message_6(client_conn):
+    pass
+
+
 def server_program():
     # get the hostname
     host = socket.gethostname()
     port = 5000  # initiate port no above 1024
+    pg_port = 5001  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -12,19 +33,24 @@ def server_program():
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+    client_conn, client_address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(client_address))
 
-    conn.close()  # close the connection
+    # Setup sub-protocol
+    recv_message_1(client_conn)
+    send_message_2(client_conn)
+
+    # Exchange sub-protocol
+    # Open connection to PG
+    pg_socket = socket.socket()  # instantiate
+    pg_socket.connect((host, pg_port))  # connect to the server
+
+    send_message_4(pg_socket)
+    recv_message_5(pg_socket)
+    send_message_6(client_conn)
+
+    pg_socket.close()
+    client_conn.close()  # close the connection
 
 
 if __name__ == '__main__':
