3912068ba2dd04ee72057cb576604d157f0fb0b7
Calin Timofte <calin.timofte@yahoo.com>
2021-02-25 11:50:58 +0200

    Removes useless payment gateway copy/pasted code Adds future payment gateway function headers

--- e-commerce/PaymentGateway.py
+++ e-commerce/PaymentGateway.py
@@ -1,6 +1,14 @@
 import socket
 
 
+def recv_message_4(merchant_conn):
+    pass
+
+
+def send_message_5(merchant_conn):
+    pass
+
+
 def server_program():
     # get the hostname
     host = socket.gethostname()
@@ -12,19 +20,14 @@ def server_program():
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+    merchant_conn, merchant_address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(merchant_address))
 
-    conn.close()  # close the connection
+    # Exchange sub-protocol
+    recv_message_4(merchant_conn)
+    send_message_5(merchant_conn)
+
+    merchant_conn.close()  # close the connection
 
 
 if __name__ == '__main__':
