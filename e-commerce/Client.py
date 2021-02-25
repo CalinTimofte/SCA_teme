6fad7e465c74f2441a7ed4b1179e6d0a8b56c04f
Calin Timofte <calin.timofte@yahoo.com>
2021-02-25 22:34:45 +0200

    Creates certificate for client
    Must fix bug in client cert creation process.

--- e-commerce/Client.py
+++ e-commerce/Client.py
@@ -1,4 +1,4 @@
-import socket
+import socket, datetime
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography import x509
 from cryptography.x509.oid import NameOID
@@ -7,11 +7,11 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-#AES keys
+# AES keys
 aes_key = b'$P\xb5I8\xcb\xd2y\xa7\xad\x8c\xb3\xb7Se\xed\xe1|\xeeu\x9e\x8f\x0f8{\xa9{sO\xc1\xfdL'
 aes_iv = b'\xcd!9\xae\xc1\xd0/Yv\xc8\x02x\xdc\x89\xa9\xa6'
 
-#RSA keys
+# RSA keys
 with open("Keys/client_rsa_priv_key.txt", "rb") as key_file:
     private_key_rsa = serialization.load_pem_private_key(
         key_file.read(),
@@ -22,6 +22,35 @@ with open("Keys/client_rsa_pub_key.txt", "rb") as key_file:
     public_key_rsa = serialization.load_pem_public_key(
         key_file.read()
     )
+
+
+def generate_cert_client():
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
+    ])
+
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        # Our certificate will be valid for 10 days
+        datetime.datetime.utcnow() + datetime.timedelta(days=10)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+        critical=False
+    ).sign(private_key_rsa, hashes.SHA256())
+
+    print(cert.public_bytes(serialization.Encoding.PEM))
+
 
 def send_message_1(client_socket):
     pass
